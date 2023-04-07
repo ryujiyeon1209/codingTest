a79e785b0c223551af22a307127fed53cc343081
ryujiyeon1209 <122500615+ryujiyeon1209@users.noreply.github.com>
2023-04-07 15:39:59 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 556 ms, Memory: 90408 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,98 +1,130 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
-	static int K; //말처럼 움직일 수 있는 수
-	static int W,H; //맵 크기
+	
+	static int chance, n, m, min;
+	static int[] r, c;
 	static int[][] map;
-	static boolean[][][] visited; //방문여부 + 점프 체크
-	static int ans = Integer.MAX_VALUE;
+	static boolean[][][] isvisited;
+//	static Queue<Monkey> queue;
 	
-	static int[] dr = {0,-1,0,1,-1,1,2,2,-1,1,-2,-2}; //좌,상,우,하
-	static int[] dc = {-1,0,1,0,-2,-2,-1,1,2,2,1,-1}; //대각선까지
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		K = sc.nextInt();
-		W = sc.nextInt();
-		H = sc.nextInt();
-		
-		map = new int[H][W];
-		visited = new boolean[H][W][K+1];
-		
-		for(int i=0; i<H; i++) { //맵 받아주기
-			for(int j=0; j<W; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		
-		bfs();
-		
-		if(ans == Integer.MAX_VALUE)
-			System.out.println("-1");
-		else
-			System.out.println(ans);
-	}
-	
-	static void bfs() {
-		Queue<Point> q = new LinkedList<Point>();
-		q.add(new Point(0,0,0,0));
-		visited[0][0][0] = true;
-		
-		
-		while(!q.isEmpty()) {
-			Point p = q.poll();
-		
-			//도착했으면 끝!
-			if(p.x == H-1 && p.y == W-1) {
-				ans = p.cnt;
-				break;
-			}
-			
-			for(int i=0; i<12; i++) {
-				int nr = p.x + dr[i];
-				int nc = p.y + dc[i];
-				
-				//칸 넘어가면 pass
-				if(nr<0 || nc<0 || nr>=H || nc>=W)
-					continue;
-				//방문할 수 없다면 pass
-				if(map[nr][nc] == 1)
-					continue;
-				//말처럼 움직이는데 점프 횟수가 남아있지 않다면 pass.
-				if(i>=4 && p.jump >= K)
-					continue;
-				//한 칸씩 이동으로 방문 한 적 있으면 pass
-				if(i<4 && visited[nr][nc][p.jump])
-					continue;
-				//말 처럼 뛰었을 때 방문 한 적 있으면 pass
-				if(i>=4 && visited[nr][nc][p.jump+1])
-					continue;
-				
-				//점프 안 하고 갈 수 있는 길은 가보기
-				if(i<4){
-					visited[nr][nc][p.jump] = true;
-					q.add(new Point(nr,nc, p.cnt+1, p.jump));
-				}
-				//점프 할 수 있으면 점프 해보기.
-				if(i>=4 && p.jump < K){					
-					visited[nr][nc][p.jump+1] = true;
-					q.add(new Point(nr,nc, p.cnt+1, p.jump+1));
-				}
-			}
-		}
-	}
-	
-	static class Point{
-		int x,y, cnt, jump;
-		Point(int x, int y, int cnt, int jump){
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-			this.jump = jump;
-		}
-	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		chance = Integer.parseInt(br.readLine());	//점프할 수 있는 횟수
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		m = Integer.parseInt(st.nextToken());	//열
+		n = Integer.parseInt(st.nextToken());	//행
+		
+		//여행길 배열 생성 및 값 입력받기, 방문처리 배열 초기화
+		map = new int[n][m];
+		isvisited = new boolean[n][m][chance+1];
+		for(int i=0; i<n; i++) {
+			st =  new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				map[i][j]= Integer.parseInt(st.nextToken());
+			}
+		}
+
+		//말처럼 뛰어다닐 수 있는 좌표 탐색
+		r= new int[] {0, 1, 0, -1, -2, -2, -1, -1, 2, 2, 1, 1};
+		c= new int[] {1, 0, -1, 0, -1, 1, -2, 2, -1, 1, -2, 2};
+		
+		min=-1;
+		BFS();
+		
+		//출력
+		bw.write(String.valueOf(min));
+		bw.close();
+		br.close();
+		
+	}
+	
+	
+	public static void BFS() {
+		
+		//큐 생성
+		 Queue<Monkey> queue = new LinkedList<Monkey>();
+		 queue.offer(new Monkey(0, 0, 0, 0));
+		
+		//큐에 넣고 방문처리
+		isvisited[0][0][0]=true;
+		
+		Outer:
+		while(!queue.isEmpty()) {
+			
+			Monkey curr = queue.poll();
+			
+			int i = curr.x;		//행
+			int j = curr.y;		//열
+			int jump = curr.jump;		//말처럼 뛴 점프 횟수
+			int depth = curr.depth;	//탐색 횟수
+			
+			if(i==n-1 && j==m-1) { min=depth; break Outer; }
+			
+
+			for(int k=0; k<12; k++) {
+				
+				int dr = i+r[k];
+				int dc = j+c[k];
+				
+				//조건1. 인덱스 범위
+				if(!(0<=dr && dr<n && 0<=dc && dc<m)) continue;
+				
+				//조건2. 장애물이면 넘어가기
+				if(map[dr][dc]==1) continue;
+				
+				//조건3. 말처럼 뛰려고 하는데 찬스를 다쓰면 넘어가기
+				if(k>=4 && jump>=chance) continue;
+				
+				//조건4. 말처럼 뛰려고 하는데 방문한 적 있으면 넘어가기
+				if(k>=4 && isvisited[dr][dc][jump+1]) continue;
+
+				//조건5. 인접한 곳을 가려고 하는데 이미 방문한 곳이면 넘어가기
+				if(k<4 && isvisited[dr][dc][jump]) continue;
+				
+				//경우의 수1. 인접한 곳을 가기!
+				if( k<4) {
+					isvisited[dr][dc][jump]=true;						//방문처리
+					queue.offer(new Monkey (dr, dc, jump, depth+1));	//큐에 넣기				
+				}
+				
+				
+				//경우의 수2. 말처럼 뛰기!
+				if( jump<chance && 4<=k) {
+					isvisited[dr][dc][jump+1]=true;						//방문처리
+					queue.offer(new Monkey (dr, dc, jump+1, depth+1));	//큐에 넣기
+				}
+					
+			}
+		}
+	}
 }
+
+class Monkey{
+	int x;
+	int y;
+	int jump;
+	int depth;
+	
+	
+	public Monkey(int x, int y, int jump, int depth) {
+		this.x = x;
+		this.y = y;
+		this.jump = jump;
+		this.depth = depth;
+	}
+}
+
+//jump가 1보다 크고 값이 0이라면, 점프를 한 적이 있고, 이 좌표 처음이야
+//jump가 1보다 크고 값이 1이라면, 점프를 한 적이 있고, 이 좌표 방문한 루트 있어
+//jump가 0이고 값이 0이라면, 점프를 한 적 없고, 좌표 처음이야
+//jump가 0이고 값이 1이라면, 점프를 한 적 없고, 이 좌표 방문한 루트가 있어
