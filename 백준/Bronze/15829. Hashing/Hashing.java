30d2fc201e6bec1d386a392b555462fcdc385da6
ryujiyeon1209 <122500615+ryujiyeon1209@users.noreply.github.com>
2023-04-17 20:48:14 +0900

    [Bronze II] Title: Hashing, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +7,13 @@ public class Main {
 		
 		//알파벳 번호 배열 만들기
 		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		//알파벳 번호 배열 맵으로 바꾸기
+		Map<Character, Integer> map = new HashMap();
+		for(int i=0; i<alphabet.length; i++) {
+			map.put(alphabet[i], (i+1));
+		}
+		
 		
 		//문자 개수 입력받기
 		int n = sc.nextInt();
@@ -16,15 +24,11 @@ public class Main {
 		//해싱 함수 공식 적용하기
 		long result=0;
 		for(int i=0; i<test.length; i++) {
-			for(int j=0; j<alphabet.length; j++) {
-				if(alphabet[j]==test[i]) {
-					result+=(((j+1)*Math.pow(31, i)));
-				}
-			}
+			result+=((map.get(test[i])*Math.pow(31, i))%1234567891);
 		}
 		
-//		System.out.println(result);
-		System.out.println(result%1234567891);
+		//출력
+		System.out.println(result);
 		
 		
 	}
