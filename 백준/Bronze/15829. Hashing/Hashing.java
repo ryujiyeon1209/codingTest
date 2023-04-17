4dad4a170e3065bfdaed84cbcbb6853c53a9c869
ryujiyeon1209 <122500615+ryujiyeon1209@users.noreply.github.com>
2023-04-17 20:40:30 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,21 +3,29 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		char[] arr = new char[N];
-		String str = sc.next();
-		char[] alpa = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-		int sum = 0;
-		for(int i=0; i<N; i++) {
-			arr[i] = str.charAt(i);
-		}
-		for(int i=0; i<N; i++) {
-			for(int j=1; j<alpa.length; j++) {
-				if(arr[i] == alpa[j]) {
-					sum += j * Math.pow(31, i);
+		
+		//알파벳 번호 배열 만들기
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		//문자 개수 입력받기
+		int n = sc.nextInt();
+		
+		//문자열 입력받기 > 문자 배열로 만들기
+		char[] test = sc.next().toCharArray();
+		
+		//해싱 함수 공식 적용하기
+		long result=0;
+		for(int i=0; i<test.length; i++) {
+			for(int j=0; j<alphabet.length; j++) {
+				if(alphabet[j]==test[i]) {
+					result+=(((j+1)*Math.pow(31, i)));
 				}
 			}
 		}
-		System.out.println(sum);
+		
+//		System.out.println(result);
+		System.out.println(result%1234567891);
+		
+		
 	}
 }
