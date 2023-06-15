58773e02ac2da709124bfb598cd1221c621ce605
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2023-06-15 21:11:15 +0900

    [Gold V] Title: 강의실 배정, Time: 1428 ms, Memory: 313668 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -12,14 +12,17 @@ public class Main {
 		//배열 생성 및 값 입력받기
 		int[][] arr = new int[n][2];
 		for(int i=0; i<n; i++) {
-			arr[i][0] = sc.nextInt();
-			arr[i][1] = sc.nextInt();
+			arr[i][0] = sc.nextInt();	//시작 시간
+			arr[i][1] = sc.nextInt();	//끝나는 시간
 		}
 		
+		//배열 정렬 
+		//1. 시작하는 시간을 기준으로 오름차순으로
+		//2. 끝나는 시간을 기준으로 오름차순으로
 		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				if(o1[0]==o2[0]) return o1[1]-o1[1];
+				if(o1[0]==o2[0]) return o1[1]-o2[1];
 				return o1[0]-o2[0];
 			}
 		});
@@ -35,6 +38,7 @@ public class Main {
 			//새롭게 끝나는 시간 큐에 넣기
 			queue.add(arr[i][1]);
 		}
+		
 		
 		//출력
 		System.out.println(queue.size());
