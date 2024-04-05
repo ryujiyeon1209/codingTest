7de2fc8450d352abe317e5567ba322b3e8dede19
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-04-05 18:05:42 +0900

    [Silver I] Title: 부등호, Time: 776 ms, Memory: 43184 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -3,83 +3,84 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int n, count;
-	static long result, max, min;
-	static String answerMax, answerMin;
-	static int[] num, choose;
-	static String[] sign;
+	static String[] arr;
 	static boolean[] isvisited;
+	static int[] num, select;
+	static String max, min;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+	
+		//부등호 개수 입력받기
+		int n = sc.nextInt();
 		
-		//부등호 개수
-		n = sc.nextInt();
-		
-		//부등호 배열
-		sign = new String[n];
-		for(int i=0; i<n; i++) {
-			sign[i]=sc.next();
+		//부등호 저장할 배열
+		arr = new String[n];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.next();
 		}
 		
-		//1-9 숫자 배열
-		num = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-		choose = new int[10];
+		num = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		isvisited = new boolean[num.length];	//방문체크 배열
+		select = new int[n+1];					//9개중 고른 숫자를 담을 배열
 		
-		isvisited = new boolean[10];
-
-		//최소값 최대값 초기화도 long타입으로 해주기
-		max=Long.MIN_VALUE;
-		min=Long.MAX_VALUE;
-		Solution(0);
+		max = "0"; min = "9876543210";
+		permutation(0);
 		
-		System.out.println(answerMax);
-		System.out.println(answerMin);
-
+		//출력
+		System.out.println(max +"\n"+ min);
 	}
 	
-	//n개중에 n개 고르기 > 순서 영향O > 순열
-	public static void Solution(int idx) {
+	
+	//순열
+	public static void permutation(int idx) {
 		
-		
-		if(idx>=10) {
+		if(select.length<=idx) {
 			
-			//숫자 10개중에 부등호 개수 +1개만 str에 넣기
-			String str="";
-			for(int i=0; i<n+1; i++) { str+=choose[i]; }
-			
-			//부등호에 맞는지 확인하기
-			Boolean flag=true;
-			
-			for(int i=0; i<n; i++) {
-				if(sign[i].equals(">")) { 
-					if(!(choose[i]>choose[i+1])) { flag=false; break;}
-				} else {
-					if(!(choose[i]<choose[i+1])) { flag=false; break;}
+			//부등호가 맞는지 확인
+			boolean flag = true;
+			for(int i=0; i<arr.length; i++) {
+				String type = arr[i];
+				
+				if(type.equals("<") && select[i]>=select[i+1]) { 
+					flag = false; 
+					break; 
+				} else if(type.equals(">") && select[i]<=select[i+1]) {
+					flag = false;
+					break;
 				}
 			}
 			
-			//최소값 최대값 비교
+			//부등호가 맞다면,
 			if(flag) {
-				result=Long.parseLong(str);
-
-				if(result>max) { max=result; answerMax=str;}
-				if(result<min) { min=result; answerMin=str;}
+				
+				String str = "";
+				for(int i=0; i<select.length; i++) {
+					str+=select[i];
+				}
+				
+				//최대 최소값 비교
+				if(Long.parseLong(str)<Long.parseLong(min)) {
+					min = str;
+				}
+				if(Long.parseLong(max)<Long.parseLong(str)) {
+					max = str;
+				}
 			}
+			
 			return;
 		}
 		
-        
-		//순열 코드
-		for(int i=0; i<10; i++) {
+		
+		for(int i=0; i<num.length; i++) {
 			if(isvisited[i]) continue;
 			
-			isvisited[i]=true;
-			choose[idx]=num[i];
-			Solution(idx+1);
+			isvisited[i] = true;
 			
-			isvisited[i]=false;
+			select[idx] = num[i];
+			permutation(idx+1);
 			
+			isvisited[i] = false;
 		}
 	}
 }
