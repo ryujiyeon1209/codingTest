e3eaaf7f83db76c792f7a3189d01a8df07e33d32
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-04-09 17:22:37 +0900

    [Silver III] Title: 게임, Time: 112 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//게임 횟수
+		Long x = sc.nextLong();	
+		
+		//이긴 횟수
+		Long y = sc.nextLong();	
+		
+		//이긴 승률
+		Long win = (y*100)/x;	
+		
+		//이분탐색
+		Long answer = Long.MAX_VALUE;
+		
+		Long min = x;
+		Long max = 1000000000000L;
+		
+		while(min<=max) {
+			Long mid = (min+max)/2;
+			
+			if(((mid-x+y)*100/(mid))<=win) min = mid+1;
+			else {
+				answer = Math.min(answer, mid);
+				max = mid-1;
+			}
+		}
+		
+		//출력
+		if(answer==Long.MAX_VALUE) System.out.println(-1); 
+		else System.out.println(answer-x);
+	}
+}
