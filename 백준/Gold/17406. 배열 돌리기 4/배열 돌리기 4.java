7842612e848b2000ee389428c36d99b3ba31e4ec
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-06-11 21:48:05 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 364 ms, Memory: 33044 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -2,146 +2,138 @@ import java.util.*;
 
 public class Main {
 	
-	static int n, m, k, min;
-	static int[][] arr, tmp;
-	static List<Integer>[] list, choose;
+	static int h, w, k, answer;
+	static int[] choose;
+	static int[][] map, arr;
 	static boolean[] isvisited;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		k = sc.nextInt();
+		h = sc.nextInt();	//높이
+		w = sc.nextInt();	//너비
+		k = sc.nextInt();	//회전 수
 		
-		//배열 생성 및 값 입력받기
-		arr = new int[n+1][m+1];
-		for(int i=1; i<n+1; i++) {
-			for(int j=1; j<m+1; j++) {
-				arr[i][j]=sc.nextInt();
+		//배열 정보 받기
+		map = new int[h][w];
+		for(int i=0; i<h; i++) {
+			for(int j=0; j<w; j++) {
+				map[i][j] = sc.nextInt();
 			}
 		}
 		
-		//임시 배열 복사
-		tmp = new int[n+1][m+1];
-		for(int i=0; i<n+1; i++) {
-			tmp[i]=arr[i].clone();
-		}
-		
-		//회전 리스트 배열 생성
-		list = new ArrayList[k];
-		choose = new ArrayList[k];
-		
+		//회전 정보 받기
+		arr = new int[k][3];
 		for(int i=0; i<k; i++) {
-			list[i]=new ArrayList();
-
-			list[i].add(sc.nextInt());	//기준 r
-			list[i].add(sc.nextInt());	//기준 c
-			list[i].add(sc.nextInt());	//범위 s
+			arr[i][0] = sc.nextInt()-1;
+			arr[i][1] = sc.nextInt()-1;
+			arr[i][2] = sc.nextInt();
 		}
 		
-		//방문 체크 배열
+		choose = new int[k];
 		isvisited = new boolean[k];
 		
-		min=Integer.MAX_VALUE;
+		answer = Integer.MAX_VALUE;
 		permutation(0);
 		
 		//출력
-		System.out.println(min);
+		System.out.println(answer);
 	}
-	
 	
 	//순열
 	public static void permutation(int idx) {
 		
-		if(idx>=k) {
-			for(int i=0; i<k; i++) {
-				Solution(choose[i].get(0), choose[i].get(1), choose[i].get(2));
-			}
-			
-			//최소값 구하기
-			for(int i=1; i<n+1; i++) {
-				int sum=0;
-				for(int j=1; j<m+1; j++) {
-					sum+=arr[i][j];
+		if(k<=idx) {
+			//배열 복사
+			int[][] tmp = new int[h][w];
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					tmp[i][j] = map[i][j];
 				}
-				min=Math.min(min, sum);
 			}
 
-			//다시 tmp값을 arr로 복사
-			for(int i=0; i<n+1; i++) {
-				arr[i]=tmp[i].clone();
+			//회전 메서드
+			for(int t=0; t<choose.length; t++) {
+				int r = arr[choose[t]][0];
+				int c = arr[choose[t]][1];
+				int s = arr[choose[t]][2];
+				
+				while(0<s) {
+					tmp = rotation(tmp, r, c, s);
+					s--;
+				}
 			}
+			
+			//배열 a 계산
+			int result = check(tmp);
+			answer = Math.min(answer, result);
 			return;
 		}
 		
-        
-		for(int i=0; i<k; i++) {
+		
+		for(int i=0; i<choose.length; i++) {
 			if(isvisited[i]) continue;
 			
 			isvisited[i]=true;
-			choose[idx]=list[i];
+			choose[i] = idx;
+			
 			permutation(idx+1);
-			
 			isvisited[i]=false;
-			
-		}
-	} 
-	
-	
-	//배열 옮기기
-	public static void Solution(int r, int c, int s) {
-		
-		//큐 생성 
-		Queue<Integer> queue = new LinkedList();
-
-		int startR = r-s;
-		int startC = c-s;
-		int endR = r+s;
-		int endC = c+s;
-		
-		while(startR<endR) {
-
-			//왼쪽에서 오른쪽
-			for(int j=startC; j<=endC; j++) {
-				if(queue.isEmpty()) queue.add(arr[startR][j]);
-				else {
-					queue.add(arr[startR][j]);
-					arr[startR][j]=queue.poll();
-				}
-			}
-			
-			//위에서 아래로
-			for(int j=startR+1; j<=endR; j++) {
-				if(queue.isEmpty()) queue.add(arr[j][endC]);
-				else {
-					queue.add(arr[j][endC]);
-					arr[j][endC]=queue.poll();
-				}
-			}
-			
-			//오른쪽에서 왼쪽으로
-			for(int j=endC-1; j>=startC; j--) {
-				if(queue.isEmpty()) queue.add(arr[endR][j]);
-				else {
-					queue.add(arr[endR][j]);
-					arr[endR][j]=queue.poll();
-				}
-			}
-			
-			//아래서 위로
-			for(int j=endR-1; j>=startR; j--) {
-				if(queue.isEmpty()) queue.add(arr[j][startC]);
-				else {
-					queue.add(arr[j][startC]);
-					arr[j][startC]=queue.poll();
-				}
-            }
-			
-			startR++;
-			startC++;
-			endR--;
-			endC--;
 		}
 	}
+	
+	//배열 돌리기
+	public static int[][] rotation(int[][] tmp, int r, int c, int s) {
+		Queue<Integer> queue = new LinkedList<>();
+		
+		int startr = r-s;
+		int startc = c-s;
+		
+		int endr = r+s;
+		int endc = c+s;
+			
+		int start = tmp[startr][startc];
+		
+		//위쪽에서 아래쪽
+		for(int i=startr+1; i<=endr; i++) {
+			tmp[i-1][startc] = tmp[i][startc];
+		}
+
+		//왼쪽에서 오른쪽
+		for(int i=startc+1; i<=endc; i++) {
+			tmp[endr][i-1] = tmp[endr][i];
+		}
+		
+		//아래쪽에서 위쪽으로
+		for(int i=endr-1; startr<=i; i--) {
+			tmp[i+1][endc] = tmp[i][endc];
+		}
+		
+		//오른쪽에서 왼쪽으로
+		for(int i=endc-1; startc<=i; i--) {
+			if(i==startc) tmp[startr][i+1]=start;
+			else tmp[startr][i+1] = tmp[startr][i];
+		}
+
+		return tmp;
+	}
+	
+	
+	//배열 a값 구하기
+	public static int check(int[][] tmp) {
+		
+		int min = Integer.MAX_VALUE;
+		for(int i=0; i<h; i++) {
+			
+			int sum = 0;
+			for(int j=0; j<w; j++) {
+				sum+=tmp[i][j];
+			}
+			
+			min = Math.min(min, sum);
+		}
+		
+		return min;
+	}
+	
 }
