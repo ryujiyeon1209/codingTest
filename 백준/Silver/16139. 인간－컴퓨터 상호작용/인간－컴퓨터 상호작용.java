2f807ee6b0a6ddf8c93226a5bdb14b6b7f55d188
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-07-15 11:45:06 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 568 ms, Memory: 126552 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,38 +1,41 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		char[] word = sc.next().toCharArray();
+		char[] word = br.readLine().toCharArray();
 		
-		List<Integer>[] list = new ArrayList[26];
-		for(int i=0; i<list.length; i++) {
-			list[i] = new ArrayList();
-		}
-		
+        //누적합
+		int[][] dp = new int[26][word.length+1];
 		for(int i=0; i<word.length; i++) {
-			list[word[i]-'a'].add(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-
-		int test = sc.nextInt();
-		for(int t=0; t<test; t++) {
-			char ch = sc.next().charAt(0);
 			
-			int start = sc.nextInt();
-			int end = sc.nextInt();
+			int idx = word[i]-'a';
+			
+			for(int j=0; j<dp.length; j++) {
+				if(j==idx) dp[j][i+1]= dp[j][i]+1;
+				else dp[j][i+1]= dp[j][i];
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		
+        int test = Integer.parseInt(br.readLine());
+		for(int t=0; t<test; t++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str); 
+			
+			char ch = st.nextToken().charAt(0);
+			
+			int start = Integer.parseInt(st.nextToken())+1;
+			int end = Integer.parseInt(st.nextToken())+1;
 			
 			int idx = ch-'a';
-			int count = 0;
-			for(int i=0; i<list[idx].size(); i++) {
-				if(start<=list[idx].get(i) && list[idx].get(i)<=end) count++;
-			}
-			
-			sb.append(count+ "\n");
+			sb.append((dp[idx][end]-dp[idx][start-1])+ "\n");
 		}
 		
+        //출력
 		System.out.println(sb.toString());
 	}
 }
