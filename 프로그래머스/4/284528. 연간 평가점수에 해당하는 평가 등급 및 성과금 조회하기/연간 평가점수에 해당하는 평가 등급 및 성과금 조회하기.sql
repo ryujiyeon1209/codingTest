5dcdadcb8c3eb6960151b80617cab8561981181f
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-07-28 18:02:49 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,11 +1,8 @@
 -- 코드를 작성해주세요
-select EMP_NO, EMP_NAME, GRADE, 
-    if(grade="S", sal*0.2, if(grade="A", sal*0.15, if(grade="B", sal*0.1, 0))) as BONUS
-from(
-    select e.emp_no, e.emp_name, e.sal,
-        if(96<=avg(g.score), "S", if(90<=avg(g.score), "A", if(80<=avg(g.score), "B", "C"))) as grade
-    from HR_DEPARTMENT as d join HR_EMPLOYEES as e join HR_GRADE as g
-    where d.dept_id=e.dept_id and e.emp_no=g.emp_no
-    group by e.emp_no
-) as t
+select e.emp_no as EMP_NO, e.emp_name as EMP_NAME, 
+    if(96<=avg(g.score), "S", if(90<=avg(g.score), "A", if(80<=avg(g.score), "B", "C"))) as GRADE,
+    if(96<=avg(g.score), sal*0.2, if(90<=avg(g.score), sal*0.15, if(80<=avg(g.score), sal*0.1, 0))) as BONUS
+from HR_DEPARTMENT as d join HR_EMPLOYEES as e join HR_GRADE as g
+where d.dept_id=e.dept_id and e.emp_no=g.emp_no
+group by e.emp_no
 order by EMP_NO;
