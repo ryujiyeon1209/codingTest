6a535d40b832d59d5e2ce8efaf4fb47686b0acfc
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-07-28 22:14:28 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
-select o.animal_id, o.name from animal_outs as o join animal_ins as i 
-where o.animal_id=i.animal_id and o.datetime<i.datetime 
-order by i.datetime asc 
+select ins.animal_id as ANIMAL_ID, ins.name as NAME
+from ANIMAL_INS as ins join ANIMAL_OUTS as outs
+where ins.animal_id=outs.animal_id
+    and outs.datetime<ins.datetime
+order by ins.datetime;
