fffe74aa71c3c729828a8658c33bfffdcc4a70ea
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-08-03 14:52:43 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-select cart_id
-from CART_PRODUCTS
-where name="Milk" or name="Yogurt"
+select CART_ID
+from CART_PRODUCTS 
+where NAME="Yogurt" or NAME="Milk"
 group by cart_id
-having 2<=count(distinct(name))
-order by cart_id;
+having 2<=count(distinct NAME)
+order by CART_ID;
