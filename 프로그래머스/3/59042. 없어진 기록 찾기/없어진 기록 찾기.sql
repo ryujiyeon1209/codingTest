a3b7b0afde46cc1cb07de95c0e3ec97b151ef67b
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-08-03 13:39:06 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,8 +1,6 @@
 -- 코드를 입력하세요
-select outs.animal_id as ANIMAL_ID, outs.name as NAME
-from ANIMAL_OUTS as outs left outer join ANIMAL_INS as ins 
-on outs.animal_id=ins.animal_id
-where ins.animal_id is null
-order by outs.animal_id;
-
-
+select outs.ANIMAL_ID, outs.NAME
+from  ANIMAL_OUTS as outs left outer join ANIMAL_INS as ins
+on ins.ANIMAL_ID=outs.ANIMAL_ID
+where ins.ANIMAL_ID is null
+order by outs.ANIMAL_ID;
