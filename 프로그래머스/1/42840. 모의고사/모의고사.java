102ac6a8facd3ec8cd6b4682e18c23885c60ad6f
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-09 15:14:34 +0900

    [level 1] Title: 모의고사, Time: 0.93 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,49 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
         
-        //2번 수포자, 3번 수포자 정답 배열 만들기
-        int[] onans = {1, 2, 3, 4, 5};
-        int[] twans = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] thans = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] an1 = {1, 2, 3, 4, 5};
+        int[] an2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] an3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] one = new int[answers.length];
-        for(int i=0; i<one.length; i++){
-            one[i] = onans[(i%onans.length)];;
+        int[] score = new int[3];
+        for(int i=0; i<answers.length; i++){
+            if(answers[i]==an1[i%5]) score[0]++;
+            if(answers[i]==an2[i%8]) score[1]++;
+            if(answers[i]==an3[i%10]) score[2]++;
         }
         
-        int[] two = new int[answers.length];
-        for(int i=0; i<two.length; i++){
-            two[i] = twans[(i%twans.length)];
+        int max = 0;
+        for(int i=0; i<score.length; i++){
+            if(max<score[i]) max=score[i];
         }
         
-        int[] three = new int[answers.length];
-        for(int i=0; i<three.length; i++){
-            three[i] = thans[(i%thans.length)];
+        int count = 0;
+        for(int i=0; i<score.length; i++){
+            if(max==score[i]) count++;
         }
         
-        //정답 체크하기
-        int[] count = new int[3];
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i]==one[i]) count[0]++;
-            if(answers[i]==two[i]) count[1]++;
-            if(answers[i]==three[i]) count[2]++;
-        }
-        
-        int max = Math.max(count[0], Math.max(count[1], count[2]));
-        
-        int size = 0;
-        for(int i=0; i<count.length; i++){
-            if(count[i]==max) size++;
-        }
-        
-        //배열 생성
-        int[] answer = new int[size];
-        
-        int idx = 0;
-        for(int i=0; i<count.length; i++){
-            if(count[i]==max) answer[idx++] = i+1; 
+        int idx=0;
+        int[] answer = new int[count];
+        for(int i=0; i<score.length; i++){
+            if(max==score[i]) answer[idx++]=i+1;
         }
         
         return answer;
