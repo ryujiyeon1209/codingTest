7d6a00dfba15a32e328a2a9c11ad4018443b989a
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-09 23:57:07 +0900

    [level 2] Title: 방문 길이, Time: 0.12 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,44 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int solution(String dirs) {
         int answer = 0;
         
-        //출발지점
-        int r = 5;
-        int c = 5;      
+        boolean[][][] map = new boolean[4][11][11];
         
-        //위아래양옆
-        boolean[][][] isvisited = new boolean[4][11][11];
-            
-        //명령어 확인
-        for(int i=0; i<dirs.length(); i++){
-            
-            if(dirs.charAt(i)=='U' && 0<=r-1) {
-                if(!isvisited[0][r][c] && !isvisited[1][r-1][c]) answer++; 
-                isvisited[0][r][c]=true;
-                
-                r--;
+        int r = 5;
+        int c = 5;
+        
+        //계산하기
+        char[] charr = dirs.toCharArray();
+        for(int i=0; i<charr.length; i++){
+            if(charr[i]=='U' && 0<=r-1) {
+                if(!map[1][r-1][c] && !map[0][r][c]) answer++;
+                map[0][r--][c]=true;
             }
-            else if(dirs.charAt(i)=='D' && r+1<11) {                
-                if(!isvisited[1][r][c] && !isvisited[0][r+1][c]) answer++;
-                isvisited[1][r][c]=true;
-                
-                r++;
-            } 
-            else if(dirs.charAt(i)=='R' && c+1<11) {                
-                if(!isvisited[2][r][c] && !isvisited[3][r][c+1]) answer++; 
-                isvisited[2][r][c]=true;
-                
-                c++;
+            if(charr[i]=='D' && r+1<=10) {
+                if(!map[0][r+1][c] && !map[1][r][c]) answer++;
+                map[1][r++][c]=true;
             }
-            else if(dirs.charAt(i)=='L' && 0<=c-1) {                 
-                if(!isvisited[3][r][c] && !isvisited[2][r][c-1]) answer++;
-                isvisited[3][r][c]=true;
-                
-                c--;
+            if(charr[i]=='R' && c+1<=10) {
+                if(!map[3][r][c+1] && !map[2][r][c]) answer++;
+                map[2][r][c++]=true;
             }
-
+            if(charr[i]=='L' && 0<=c-1) {
+                if(!map[2][r][c-1] && !map[3][r][c]) answer++;
+                map[3][r][c--]=true;
+            }
         }
         
         return answer;
