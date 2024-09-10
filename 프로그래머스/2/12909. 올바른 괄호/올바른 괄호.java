35d3592e5e95c18f2d0bb8827ddb2bb98dcccebb
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-10 09:53:43 +0900

    [level 2] Title: 올바른 괄호, Time: 19.68 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,24 +4,16 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        char[] charr = s.toCharArray();
-        
         Stack<Character> stack = new Stack();
-        for(int i=0; i<charr.length; i++){
-            if(charr[i]=='(') {
-                stack.add('(');
-            }
-            else if(!stack.isEmpty() && charr[i]==')') {
-                stack.pop();
-            }
-            else if(stack.isEmpty() && charr[i]==')') { 
-                answer=false;
-                break;
-            }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch=='(') stack.push('(');
+            else if(!stack.isEmpty() && ch==')') stack.pop();
+            else if(stack.isEmpty() && ch==')') return false;
         }
         
-        if(stack.size()!=0) answer=false;
-        
+        if(!stack.isEmpty()) answer=false;
         return answer;
     }
 }
