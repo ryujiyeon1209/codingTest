f694e52e627b36f40bfed64becbeb002e43abb0e
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-10 11:41:07 +0900

    [level 2] Title: 괄호 회전하기, Time: 43.49 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,51 +4,34 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        char[] chList = s.toCharArray();
-        
-        Queue<Character> queue = new LinkedList();
-        for(int i=0; i<s.length(); i++){
-            queue.add(chList[i]);
-        }
-        
-        for(int i=0; i<chList.length; i++){
-            boolean flag = isCorrect(queue);
-            if(flag) answer++;
+        int n = 0;
+        while(n<s.length()) {
             
-            char ch = queue.poll();
-            queue.add(ch);
+            Stack<Character> stack = new Stack();
+            
+            boolean flag =true;
+            for(int i=0; i<s.length(); i++){
+                char ch = s.charAt((i+n)%s.length());
+                
+                if(ch=='(') stack.push('(');
+                else if(ch==')' && !stack.isEmpty() && stack.peek()=='(') stack.pop();
+                
+                else if(ch=='[') stack.push('[');
+                else if(ch==']' && !stack.isEmpty() && stack.peek()=='[') stack.pop();
+                
+                else if(ch=='{') stack.push('{');
+                else if(ch=='}' && !stack.isEmpty() && stack.peek()=='{') stack.pop();
+                
+                else { flag=false; break; }
+                  
+            }
+           
+            if(!stack.isEmpty()) flag=false;
+            if(flag) answer++;
+    
+            n++;
         }
-
         
         return answer;
     }
-    
-    public boolean isCorrect(Queue<Character> queue){
-        Stack<Character> stack = new Stack();
-            
-        Queue<Character> tmpQueue = new LinkedList(queue);
-        while(!tmpQueue.isEmpty()){
-            char ch = tmpQueue.poll();
-            
-            if(stack.isEmpty() && !(ch=='(' || ch=='{' || ch=='[')) return false;
-            else if(stack.isEmpty() && (ch=='(' || ch=='{' || ch=='[')) stack.push(ch);
-                        
-            else if((stack.peek()=='(' && ch==')') ||
-                    (stack.peek()=='{' && ch=='}') || (stack.peek()=='[' && ch==']')) stack.pop();
-            
-            else if(stack.peek()=='(' && (ch=='}' || ch==']')) return false;
-            else if(stack.peek()=='{' && (ch==')' || ch==']')) return false;
-            else if(stack.peek()=='[' && (ch==')' || ch=='}')) return false;
-            
-            else if(stack.peek()=='(' && (ch== '(' || ch=='{' || ch=='[')) stack.push(ch);
-            else if(stack.peek()=='{' && (ch== '(' || ch=='{' || ch=='[')) stack.push(ch);
-            else if(stack.peek()=='[' && (ch== '(' || ch=='{' || ch=='[')) stack.push(ch);
-
-        }
-        
-        if(0<stack.size()) return false;
-        return true;
-    }
-    
-    
 }
