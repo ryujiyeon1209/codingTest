a968d785d931a4c466c508fae585ef57d3f0d407
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-10 13:06:57 +0900

    [level 2] Title: 주식가격, Time: 37.67 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,26 +7,21 @@ class Solution {
         Stack<Integer> stack = new Stack();
         for(int i=0; i<prices.length; i++){
             
-            //stack이 비었다면
-            if(stack.isEmpty()) stack.add(i);
-            
-            //다음 날에 주식이 올랐다면
-            else if(prices[stack.peek()]<=prices[i]) stack.add(i);
-            
-            //다음 날에 주식이 떨어졌다면
-            else if(prices[stack.peek()]>prices[i]) {
-                while(!stack.isEmpty() && prices[stack.peek()]>prices[i]) answer[stack.peek()]= i-stack.pop();
-                stack.add(i);
+            //전과 비교했을 때 현재 가격이 더 낮다면, 
+            while (!stack.isEmpty() && prices[i]<prices[stack.peek()]) {
+                answer[stack.peek()] = i-stack.pop();
             }
+            
+            //낮거나 그 외
+            stack.push(i);
         }
         
-        int mday = 0;
-        if(!stack.isEmpty()) mday=stack.peek();
+        int max = 0;
+        if(!stack.isEmpty()) max = stack.pop(); 
         while(!stack.isEmpty()) {
-            answer[stack.peek()] = mday-stack.pop();
+            answer[stack.peek()] = max-stack.pop();
         }
-        
         
         return answer;
     }
-}
+}  
