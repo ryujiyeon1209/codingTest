2369b5663fe60b0b4e07b778bce33839bc6343e0
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-11 15:15:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.15 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
         String answer = "";
         
-        //참여한 선수 map에 저장
         Map<String, Integer> map = new HashMap();
         for(int i=0; i<participant.length; i++){
             map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
         }
         
-        //완주한 선수 map에서 삭제하기
         for(int i=0; i<completion.length; i++){
-            map.put(completion[i], map.get(completion[i])-1);            
-            if(map.get(completion[i])==0) map.remove(completion[i]);
+            if(1<map.get(completion[i])) map.put(completion[i], map.get(completion[i])-1);
+            else map.remove(completion[i]);
         }
-        
-        //map에 마지막 남은 이름 answer에 저장
-        for(String name : map.keySet()) {
-            answer=name;    
+
+        for(String name : map.keySet()){
+            answer = name;
         }
         
         return answer;
