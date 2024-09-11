5cb4177060aa2e8a20cfcbadca0021e1ea2f268e
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-11 15:48:48 +0900

    [level 2] Title: 할인 행사, Time: 33.41 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,38 +4,35 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        //1일부터 10일 초기화
+        Map<String, Integer> map = new HashMap();
+        for(int i=0; i<want.length; i++){
+            map.put(want[i], number[i]);
+        }
+        
+        //슬라이딩 윈도우
+        int count=10;
         for(int i=0; i<10; i++){
-            for(int j=0; j<want.length; j++){
-                if(discount[i].equals(want[j])) number[j]--;
+            if(map.containsKey(discount[i])) {
+                if(0<=map.get(discount[i])-1) count--;
+                map.put(discount[i], map.get(discount[i])-1);
             }
         }
         
-        //할인 받을 수 있나 확인
-        boolean isempty = true;
-        for(int j=0; j<number.length; j++){
-            if(number[j]!=0) isempty=false;    
-        }
-        if(isempty) answer++;
-    
-        
-        //10일부터 하루씩 반복문 
+        if(count==0) answer++;
         for(int i=10; i<discount.length; i++){
             
-            //그 전 물건 초기화, 새로운 날 할인 확인
-            for(int j=0; j<want.length; j++){
-                if(discount[i-10].equals(want[j])) number[j]++;
-                if(discount[i].equals(want[j])) number[j]--;
+            if(map.containsKey(discount[i-10])) {
+                if(0<map.get(discount[i-10])+1) count++;
+                map.put(discount[i-10], map.get(discount[i-10])+1);
             }
             
-            //모든 물건을 할인 받을 수 있나 확인
-            isempty = true;
-            for(int j=0; j<number.length; j++){
-                if(number[j]!=0) isempty=false;    
+            if(map.containsKey(discount[i])) {
+                if(0<=map.get(discount[i])-1) count--;
+                map.put(discount[i], map.get(discount[i])-1);
             }
-            if(isempty) answer++;
+            
+            if(count==0) answer++;
         }
-        
         
         return answer;
     }
