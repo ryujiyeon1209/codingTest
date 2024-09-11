e45b47815bc5b1f2ab4e17ea069576658d7733c8
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-11 22:39:57 +0900

    [level 1] Title: 신고 결과 받기, Time: 199.08 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,38 +2,49 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
         
-        //이용자 정리
-        Map<String, Integer> map = new HashMap();
+        //인덱스 찾기
+        Map<String,Integer> find = new HashMap();
         for(int i=0; i<id_list.length; i++){
-            map.put(id_list[i], i);
+            find.put(id_list[i], i);
         }
         
-        //신고한 사람과 신고당한 사람을 담을 list
-        List<String>[] list = new ArrayList[id_list.length];
+        //신고 현황 중복 제거
+        Set<String> set = new HashSet();
+        for(int i=0; i<report.length; i++){
+            set.add(report[i]);
+        }
+        
+        //신고 당한 사람, 신고한 사람 인접리스트
+        List<Integer>[] list = new List[id_list.length];
         for(int i=0; i<list.length; i++){
             list[i] = new ArrayList();
         }
         
-        //신고 정리하기 : 누구한테 신고당했는지 list에 담기
-        for(int i=0; i<report.length; i++){
-            String[] cur = report[i].split(" ");
-
-            if(!list[map.get(cur[1])].contains(cur[0]))
-                list[map.get(cur[1])].add(cur[0]);
+        //신고 당한 횟수 저장
+        Map<String,Integer> map = new HashMap();
+        for(String info : set) {
+            String[] strarr = info.split(" ");
+            map.put(strarr[1], map.getOrDefault(strarr[1], 0)+1);
+            
+            int idx = find.get(strarr[1]);
+            list[idx].add(find.get(strarr[0]));
         }
         
-        //신고한 사람의 answer +1하기
-        for(int i=0; i<list.length; i++){
-            if(list[i].size()<k) continue;
-            
-            for(int j=0; j<list[i].size(); j++){
-                String name = list[i].get(j);
-                answer[map.get(name)]++;
+        
+        int[] answer = new int[id_list.length];
+        
+        //k번 이상 신고 당한 사람 찾기
+        for(String name : map.keySet()){
+            if(k<=map.get(name)) {
+                
+                int idx = find.get(name);
+                for(int i=0; i<list[idx].size(); i++){
+                    answer[list[idx].get(i)]++;
+                }
+                
             }
         }
-        
         
         return answer;
     }
