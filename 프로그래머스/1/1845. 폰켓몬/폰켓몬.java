783d30a0a85091678a28ea92ca69fc4a486b0d9e
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-14 16:01:45 +0900

    [level 1] Title: 폰켓몬, Time: 5.63 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,15 +4,13 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        //포켓몬 종류 개수 구하기
         Map<Integer, Integer> map = new HashMap();
         for(int i=0; i<nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
         
-        //최대 가질 수 있는 종류 구하기
-        if(map.size()<=nums.length/2) answer = map.size();
-        else answer = nums.length/2;
+        if(nums.length/2<=map.size()) answer = nums.length/2;
+        else answer = map.size();
         
         return answer;
     }
