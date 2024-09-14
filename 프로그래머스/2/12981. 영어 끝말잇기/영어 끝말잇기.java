a78f322ab59c63cb342a6c423a122262b9478533
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-09-14 16:22:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,38 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
+        int[] answer = new int[2];
 
-        Queue<String> queue = new LinkedList();
-        Set<String> set = new HashSet();
+        int lose=-1;
         
-        String lastCh = words[0].substring(words[0].length()-1);
-        set.add(words[0]);
+        Map<String, Integer> map = new HashMap();
+        map.put(words[0], 1);
         
-        boolean flag = true;
         for(int i=1; i<words.length; i++){
+            char pre = words[i-1].charAt(words[i-1].length()-1);
+            char cur = words[i].charAt(0);
             
-            String startCh = words[i].substring(0, 1);
-            if(!lastCh.equals(startCh)) flag = false;
-            
-            lastCh = words[i].substring(words[i].length()-1);
-            
-            int size = set.size();
-            set.add(words[i]);
-            if(set.size()== size) flag = false;
-            
-
-            
-            if(!flag) {
-                int num = ((i+1)%n==0) ? n : (i+1)%n;
-                System.out.println("num : " +num + ", " +(i+1));
-                if((i+1)%n==0) answer = new int[] {num, (i+1)/n};
-                else answer = new int[] {num, ((i+1)/n)+1};
-
-                break;    
+            if(pre!=cur || map.containsKey(words[i])) {
+                lose=i;
+                break;
             }
+            
+            map.put(words[i], map.getOrDefault(words[i], 0) +1);
         }
 
+        if(lose==-1) answer = new int[] {0, 0};
+        else answer = new int[] {(lose%n)+1, (lose/n)+1};
+        
         return answer;
     }
 }
