bcb47a3adeb94700c1f574d15e9c3c77d39d22a6
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-08 09:17:53 +0900

    [level 1] Title: K번째수, Time: 0.29 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,24 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-    
+        
         for(int i=0; i<commands.length; i++){
-            int start = commands[i][0]-1;
+            int start = commands[i][0];
             int end = commands[i][1];
-            int k = commands[i][2];
+            int idx = commands[i][2];
             
-            //새로운 배열 만들기
-            int[] tmp = new int[end-start];
-            int idx = 0;
-            for(int j=start; j<end; j++){
-                tmp[idx++] = array[j];
+            List<Integer> list = new ArrayList();
+            for(int j=start-1; j<end; j++){
+                list.add(array[j]);
             }
             
-            //정렬
-            Arrays.sort(tmp);
-            
-            //k번째 수 구하기
-            answer[i] = tmp[k-1];
+            Collections.sort(list);
+            answer[i] = list.get(idx-1);
         }
         
         return answer;
