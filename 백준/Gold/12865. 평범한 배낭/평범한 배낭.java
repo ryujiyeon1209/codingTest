417063184a4541b139bb8962c95b44481b318a0d
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-10 22:37:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 52804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,43 +1,41 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		//물건 개수
 		int n = sc.nextInt();
+		int m = sc.nextInt();
 		
-		//최대 무게
-		int k = sc.nextInt();
-		
-		//물건의 무게와 가치를 담을 배열
-		int[][] stuff = new int[n+1][2];
-		for(int i=1; i<stuff.length; i++) {
-			int w = sc.nextInt();
-			int v = sc.nextInt();
-			
-			stuff[i][0]=w;
-			stuff[i][1]=v;
+		int[][] arr = new int[n+1][2];
+		for(int i=1; i<arr.length; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
 		}
 		
-		//dp배열 생성 및 반복문 돌리기
-		int[][] dp = new int[n+1][k+1];
-
-		for(int i=1; i<dp.length; i++) {			//물건 인덱스
-			for(int j=0; j<dp[0].length; j++) {		//물건 무게
+		int[][] dp = new int[n+1][m+1];
+		
+		//현재 물건
+		for(int i=1; i<n+1; i++) {
+			//가방 무게 
+			for(int j=1; j<m+1; j++) {
+				int weight = arr[i][0];
 				
-				//고르지 않는다면,
-				if( j<stuff[i][0]) {	dp[i][j]=dp[i-1][j]; }
+				//가방 무게가 물건보다 작다면, 다른 물건 넣기
+				if(j<weight) {
+					dp[i][j] = dp[i-1][j];
+				}	
 				
-				//고른다면
-				else if(j>=stuff[i][0]){
-					dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-stuff[i][0]]+stuff[i][1]);
+				//가방에 현재 물건을 넣을 수 있다면, 현재 물건 가치+남는 공간 최대 가치 활용
+				//i-1인 이유 : 현재 물건의 가치가 영향이 있으면 안되므로
+				else {
+					dp[i][j]= Math.max(dp[i-1][j], arr[i][1]+dp[i-1][j-weight]);
 				}
+				
 			}
 		}
-	
+		
 		//출력
-		System.out.println(dp[n][k]);
+		System.out.println(dp[n][m]);
 	}
 }
