5b087d0a84aae211a35f9ae25b8d8c160d2c7ce9
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-13 00:11:59 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,13 +1,31 @@
 -- 코드를 입력하세요
-select m.member_name as MEMBER_NAME, r.review_text as REVIEW_TEXT, 
-    date_format(r.review_date, "%Y-%m-%d") as REVIEW_DATE
-from MEMBER_PROFILE as m join REST_REVIEW as r
-where m.member_id=r.member_id and
-    m.member_id = (
-        select member_id
-        from REST_REVIEW 
-        group by member_id
-        order by count(review_id) desc
+
+# with maxCount as (
+#     select count(r.REVIEW_ID)
+#     from MEMBER_PROFILE as m join REST_REVIEW as r
+#     on m.MEMBER_ID = r.MEMBER_ID
+#     group by m.MEMBER_ID
+#     order by count(r.REVIEW_ID) desc
+#     limit 1
+# )
+
+select m.MEMBER_NAME, r.REVIEW_TEXT, date_format(r.REVIEW_DATE, "%Y-%m-%d") as REVIEW_DATE
+from MEMBER_PROFILE as m join REST_REVIEW as r 
+on m.MEMBER_ID = r.MEMBER_ID
+where m.MEMBER_ID in (
+    select m.MEMBER_ID
+    from MEMBER_PROFILE as m join REST_REVIEW as r 
+    on m.MEMBER_ID = r.MEMBER_ID
+    group by m.MEMBER_ID
+    having count(r.REVIEW_ID) = (
+        select count(*)
+        from MEMBER_PROFILE as m join REST_REVIEW as r
+        on m.MEMBER_ID = r.MEMBER_ID
+        group by m.MEMBER_ID
+        order by count(*) desc
         limit 1
+    )
 )
-order by r.review_date, r.review_text;
+order by REVIEW_DATE, REVIEW_TEXT
+
+
