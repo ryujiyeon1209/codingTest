1017b5f067be0bd5c2fd8eb67c2c9849773b031c
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-13 15:32:31 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -2,6 +2,5 @@
 select BOARD_ID, WRITER_ID, TITLE, PRICE, 
     if(STATUS="SALE", "판매중", if(STATUS="RESERVED", "예약중", "거래완료")) as STATUS
 from USED_GOODS_BOARD 
-where date_format(created_date, "%Y")="2022"
-    and date_format(created_date, "%m")="10" and date_format(created_date, "%d")="05"
-order by board_id desc;
+where CREATED_DATE="2022-10-05"
+order by BOARD_ID desc
