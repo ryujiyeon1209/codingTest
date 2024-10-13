035914b0498945615fe94ce2199b9b8a499a1e13
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-13 15:27:43 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,14 +1,16 @@
 -- 코드를 입력하세요
-select h.history_id as HISTORY_ID, round(day*(c.daily_fee*((100-ifnull(d.discount_rate, 0))/100)), 0) as FEE
-from CAR_RENTAL_COMPANY_CAR as c  join (
-    select history_id, car_id, timestampdiff(day, START_DATE, END_DATE)+1 as day,
-        if(90<=timestampdiff(day, START_DATE, END_DATE)+1, "90일 이상",
-        if(30<=timestampdiff(day, START_DATE, END_DATE)+1, "30일 이상",
-        if(7<=timestampdiff(day, START_DATE, END_DATE)+1, "7일 이상", "0일 이상"))) as duration
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-) as h 
-on c.car_id=h.car_id 
-left outer join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as d
-on c.car_type=d.car_type and d.duration_type=h.duration 
-where c.car_type="트럭"
-order by FEE desc, HISTORY_ID desc;
+with TRUCK as (
+    select c.CAR_ID, c.CAR_TYPE, h.HISTORY_ID, c.DAILY_FEE, timestampdiff(day, h.START_DATE, h.END_DATE)+1 as RENTDAY,
+    if(90<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "90일 이상", 
+      if(30<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "30일 이상",
+        if(7<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "7일 이상", "N"))) as DURATION_TYPES
+    from CAR_RENTAL_COMPANY_CAR as c join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
+    on c.CAR_ID=h.CAR_ID
+    where c.CAR_TYPE="트럭"
+)
+
+select t.HISTORY_ID, 
+round((t.DAILY_FEE*(1-(ifnull(d.DISCOUNT_RATE, 0)/100)))*t.RENTDAY, 0) as FEE
+from CAR_RENTAL_COMPANY_DISCOUNT_PLAN as d right join TRUCK as t
+on d.CAR_TYPE=t.CAR_TYPE and d.DURATION_TYPE=t.DURATION_TYPES
+order by FEE desc, t.HISTORY_ID desc;
