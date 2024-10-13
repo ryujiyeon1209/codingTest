da9b5375a3552f4962bc6490d607d735074e63f5
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-13 15:08:17 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,10 +1,12 @@
 -- 코드를 입력하세요
-select concat("/home/grep/src/", b.board_id, "/", f.FILE_ID, f.FILE_NAME, f.FILE_EXT) as FILE_PATH
-from USED_GOODS_FILE as f join (
-    select board_id
-    from USED_GOODS_BOARD
+with maxView as (
+    select BOARD_ID
+    from USED_GOODS_BOARD 
     order by VIEWS desc
     limit 1
-) as b
-where b.board_id=f.board_id
+)
+
+select concat("/home/grep/src/", b.BOARD_ID, "/", f.FILE_ID, f.FILE_NAME, f.FILE_EXT) as FILE_PATH
+from maxView as b join USED_GOODS_FILE as f
+on b.BOARD_ID = f.BOARD_ID
 order by f.FILE_ID desc;
