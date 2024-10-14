6c89e02ea155805dbe4bb51ff71f91757b0eba62
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-14 12:42:50 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,9 +1,13 @@
 -- 코드를 입력하세요
-select u.user_id as USER_ID, nickname as NICKNAME, 
-    concat(u.city, " ", u.street_address1, " ", u.street_address2) as 전체주소, 
-    concat(substring(tlno, 1, 3), "-", substring(tlno, 4, 4), "-", substring(tlno, 8, 4))as 전화번호
-from USED_GOODS_BOARD as b right outer join USED_GOODS_USER as u
-on b.writer_id=u.user_id
-group by u.user_id
-having 3<=count(b.title)
-order by u.user_id desc;
+with WRITER as (
+    select WRITER_ID 
+    from USED_GOODS_BOARD 
+    group by WRITER_ID
+    having 3<=count(BOARD_ID)
+)
+
+select u.USER_ID, u.NICKNAME, concat(u.CITY, " ", u.STREET_ADDRESS1, " ", u.STREET_ADDRESS2) as "전체주소",
+    concat(substring(u.TLNO, 1, 3), "-", substring(u.TLNO, 4, 4), "-", substring(u.TLNO, 8, 4)) as "전화번호"
+from WRITER as w join USED_GOODS_USER as u
+on w.WRITER_ID=u.USER_ID
+order by u.USER_ID desc;
