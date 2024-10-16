6a313a9e3acf01d710c261fb8217f6b9568398f2
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-16 09:52:28 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
 select i.NAME, i.DATETIME
-from ANIMAL_INS as i left join ANIMAL_OUTS as o
+from ANIMAL_INS as i left join  ANIMAL_OUTS as o
 on i.ANIMAL_ID=o.ANIMAL_ID
 where o.ANIMAL_ID is null
 order by i.DATETIME
