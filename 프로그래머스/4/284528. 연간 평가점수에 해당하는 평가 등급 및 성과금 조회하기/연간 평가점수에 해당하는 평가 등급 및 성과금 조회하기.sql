4da3aa4a1d178685bf042a1787ab68c6689e182e
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-16 10:54:45 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,13 +1,13 @@
 -- 코드를 작성해주세요
-with AVGSCORE as (
-    select EMP_NO, avg(SCORE) as ASCORE
-    from HR_GRADE  
+with AVG_GRADE as (
+    select EMP_NO, avg(SCORE) as SCORE
+    from HR_GRADE 
     group by EMP_NO
-) 
+)
 
-select e.EMP_NO, e.EMP_NAME,
-    if(96<=s.ASCORE, "S", if(90<=s.ASCORE, "A", if(80<=s.ASCORE, "B", "C"))) as GRADE,
-    if(96<=s.ASCORE, SAL*0.2, if(90<=s.ASCORE, SAL*0.15, if(80<=s.ASCORE, SAL*0.1, 0))) as BONUS
-from HR_EMPLOYEES as e join AVGSCORE as s
-on e.EMP_NO=s.EMP_NO
-order by e.EMP_NO;
+select e.EMP_NO, e.EMP_NAME, 
+    if(96<=g.SCORE, "S", if(90<=g.SCORE, "A", if(80<=g.SCORE, "B", "C"))) as "GRADE",
+    if(96<=g.SCORE, e.SAL*0.2, if(90<=g.SCORE, e.SAL*0.15, if(80<=g.SCORE, e.SAL*0.1, 0))) as "BONUS"
+from AVG_GRADE as g join HR_EMPLOYEES as e
+on g.EMP_NO=e.EMP_NO
+order by e.EMP_NO
