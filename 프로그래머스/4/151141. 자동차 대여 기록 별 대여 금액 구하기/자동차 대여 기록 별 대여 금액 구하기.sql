5c0210f705af704bec8170c5ef03e9089c6ff685
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-17 11:06:55 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,16 +1,32 @@
 -- 코드를 입력하세요
-with TRUCK as (
-    select c.CAR_ID, c.CAR_TYPE, h.HISTORY_ID, c.DAILY_FEE, timestampdiff(day, h.START_DATE, h.END_DATE)+1 as RENTDAY,
-    if(90<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "90일 이상", 
-      if(30<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "30일 이상",
-        if(7<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "7일 이상", "N"))) as DURATION_TYPES
-    from CAR_RENTAL_COMPANY_CAR as c join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
+with CAR as (
+    select *
+    from CAR_RENTAL_COMPANY_CAR
+    where CAR_TYPE="트럭"
+)
+,
+HISTORY as (
+    select h.HISTORY_ID, c.CAR_ID, c.CAR_TYPE, timestampdiff(day, h.START_DATE, h.END_DATE)+1 as DAY,
+        if(90<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "90일 이상", 
+        if(30<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "30일 이상", 
+        if(7<=timestampdiff(day, h.START_DATE, h.END_DATE)+1, "7일 이상", "N"))) as DAYS,
+        c.DAILY_FEE
+    from CAR as c join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h 
     on c.CAR_ID=h.CAR_ID
-    where c.CAR_TYPE="트럭"
+)
+,
+DISCOUNT as (
+    select h.HISTORY_ID, h.CAR_ID, h.CAR_TYPE, h.DAY, h.DAYS, h.DAILY_FEE, 
+        if(d.DISCOUNT_RATE is null, 0, d.DISCOUNT_RATE) as DISCOUNT_RATE
+    from HISTORY as h left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as d
+    on h.CAR_TYPE=d.CAR_TYPE and h.DAYS=d.DURATION_TYPE
 )
 
-select t.HISTORY_ID, 
-round((t.DAILY_FEE*(1-(ifnull(d.DISCOUNT_RATE, 0)/100)))*t.RENTDAY, 0) as FEE
-from CAR_RENTAL_COMPANY_DISCOUNT_PLAN as d right join TRUCK as t
-on d.CAR_TYPE=t.CAR_TYPE and d.DURATION_TYPE=t.DURATION_TYPES
-order by FEE desc, t.HISTORY_ID desc;
+select HISTORY_ID, round((DAILY_FEE*((100-DISCOUNT_RATE)/100))) * DAY as FEE
+from DISCOUNT
+order by FEE desc, HISTORY_ID desc
+
+
+
+
+
