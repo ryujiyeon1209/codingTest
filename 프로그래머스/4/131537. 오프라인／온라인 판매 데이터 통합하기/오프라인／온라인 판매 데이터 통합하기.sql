e718bca77e1b817df20dad69ce9d4d45814c2e0a
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-17 10:13:12 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,16 @@
 -- 코드를 입력하세요
-select date_format(ons.SALES_DATE, "%Y-%m-%d") as SALES_DATE,
-    ons.PRODUCT_ID, ons.USER_ID, ons.SALES_AMOUNT
-from ONLINE_SALE as ons
-where year(ons.SALES_DATE)=2022 and month(ons.SALES_DATE)=3
-union
-select date_format(off.SALES_DATE, "%Y-%m-%d") as SALES_DATE,
-    off.PRODUCT_ID, null as USER_ID, off.SALES_AMOUNT
-from OFFLINE_SALE as off
-where year(off.SALES_DATE)=2022 and month(off.SALES_DATE)=3
-order by SALES_DATE, PRODUCT_ID, USER_ID;
+with TOTAL as (
+    select date_format(SALES_DATE, "%Y-%m-%d") as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+    from ONLINE_SALE 
+    where year(SALES_DATE)=2022 and month(SALES_DATE)=3
+
+    union all
+
+    select date_format(SALES_DATE, "%Y-%m-%d") as SALES_DATE, PRODUCT_ID, null, SALES_AMOUNT
+    from OFFLINE_SALE
+    where year(SALES_DATE)=2022 and month(SALES_DATE)=3
+)
+    
+select *
+from TOTAL
+order by SALES_DATE, PRODUCT_ID, USER_ID
