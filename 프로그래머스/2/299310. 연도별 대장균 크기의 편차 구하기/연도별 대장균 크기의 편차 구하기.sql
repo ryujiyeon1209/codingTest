245918dda5a0d99db6d58e598c09f9e0f4a6860e
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-17 10:22:54 +0900

    [level 2] Title: 연도별 대장균 크기의 편차 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
+++ 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
@@ -1,11 +1,11 @@
 -- 코드를 작성해주세요
-with maxSize as (
-    select year(DIFFERENTIATION_DATE) as year, max(SIZE_OF_COLONY) as smax
-    from ECOLI_DATA
+with STDEV as (
+    select year(DIFFERENTIATION_DATE) as YEAR, max(SIZE_OF_COLONY) as MSIZE
+    from ECOLI_DATA 
     group by year(DIFFERENTIATION_DATE)
 )
 
-select m.YEAR, (m.SMAX-e.SIZE_OF_COLONY) as YEAR_DEV, e.ID
-from ECOLI_DATA as e join maxSize as m
-on year(e.DIFFERENTIATION_DATE)=m.YEAR
-order by m.YEAR, YEAR_DEV
+select s.YEAR, (s.MSIZE-e.SIZE_OF_COLONY) as YEAR_DEV, e.ID
+from ECOLI_DATA as e join STDEV as s
+on year(e.DIFFERENTIATION_DATE)=s.YEAR
+order by s.YEAR,  (s.MSIZE-e.SIZE_OF_COLONY)
