193ab30f04f09355eb970be94a4a125d546da5e1
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-10-24 10:49:52 +0900

    [level 3] Title: 기지국 설치, Time: 3.07 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,39 +1,39 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
 
-        //지기국 전파 범위 배열
-        int[][] arr = new int[stations.length+2][2];
-        for(int i=1; i<arr.length-1; i++){
-            int num = stations[i-1];
-            
-            arr[i][0]=num-w;
-            arr[i][1]=num+w;
-        }
+        int start = 1;
+        int end = n;
         
-        //시작점
-        arr[0][0] = 0-w;
-        arr[0][1] = 0;
-        
-        //끝점
-        arr[arr.length-1][0] = n+1;
-        arr[arr.length-1][1] = n+1+w;
+        //기지국 설치 확인
+        for(int i=0; i<stations.length; i++){
 
-        //개수 확인하기
-        int spread = (w*2)+1;
-        for(int i=arr.length-1; 0<i; i--){
-            int start1 = arr[i][0];
-            int end1 = arr[i][1];
+            int node = stations[i];
+            int s = Math.max(1, node-w);
+            int e = Math.min(n, node+w);
+
+            //현재 좌표에 기지국 설치를 해야한다면
+            if(start<s) {        
+                answer+=(int)Math.ceil((s-start)*1.0/((2*w)+1));
+                start=e+1;
+            }
             
-            int start2 = arr[i-1][0];
-            int end2 = arr[i-1][1];
+            //현재 좌표에 기지국 설치가 되어있다면
+            else if(s<=start){
+                start=e+1;
+            }
             
-            if(start1-end2-1<=0) continue;              //지기국 전파 범위가 곂치면
-            answer+=Math.ceil((start1-end2-1)/(spread*1.0));  //지기국 전파가 없으면
+            //마지막까지 설치가 되어있다면
+            if(end<start) break;
         }
         
+        //마지막 설치
+        if(start<=end) {
+            answer+=(int)Math.ceil((end-start+1)*1.0/((2*w)+1));
+        }
+
         return answer;
     }
 }
+
+// System.out.println("start : " +start+ ", s : " +s+ ", e : " +e+ " > answer : " +answer);
