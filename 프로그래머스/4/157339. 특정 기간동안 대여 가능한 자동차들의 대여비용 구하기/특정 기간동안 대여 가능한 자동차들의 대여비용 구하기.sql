fe3dda90763764f30d0cd90a1e4976d4810043a3
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2024-11-01 15:55:48 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,31 +1,24 @@
 -- 코드를 입력하세요
 with CAR as (
-    select *
+    select CAR_ID, CAR_TYPE, DAILY_FEE
     from CAR_RENTAL_COMPANY_CAR 
-    where CAR_TYPE="SUV" or CAR_TYPE="세단"
+    where CAR_TYPE='세단' or CAR_TYPE='SUV'
 ),
 
-HISTORY as (
-    select distinct CAR_ID
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    where CAR_ID not in (
-        select CAR_ID
-        from CAR_RENTAL_COMPANY_RENTAL_HISTORY  
-        where START_DATE<='2022-11-30' and '2022-11-01'<=END_DATE	
-    ) 
-),
-
-DISCOUNT as (
-    select *
-    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
-    where DURATION_TYPE="30일 이상"
+PLAN as (
+    select c.CAR_ID, c.CAR_TYPE, c.DAILY_FEE, p.DISCOUNT_RATE
+    from CAR as c join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
+    where c.CAR_TYPE=p.CAR_TYPE and p.DURATION_TYPE="30일 이상"
 )
 
-select c.CAR_ID, c.CAR_TYPE, round((c.DAILY_FEE*((100-d.DISCOUNT_RATE)/100))*30) as FEE 
-from CAR as c join HISTORY as h join DISCOUNT as d
-on c.CAR_ID=h.CAR_ID and c.CAR_TYPE=d.CAR_TYPE
-where 500000<=(c.DAILY_FEE*((100-d.DISCOUNT_RATE)/100))*30 and (c.DAILY_FEE*((100-d.DISCOUNT_RATE)/100))*30<2000000
-order by FEE desc, c.CAR_TYPE, c.CAR_ID desc
-
-
-
+select CAR_ID, CAR_TYPE,
+    ceil((DAILY_FEE*(100-DISCOUNT_RATE))/100)*30 as FEE
+from PLAN
+where CAR_ID not in (
+    select CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY  
+    where START_DATE<="2022-11-30" and "2022-11-01"<=END_DATE
+)
+and 500000<=ceil((DAILY_FEE*(100-DISCOUNT_RATE))/100)*30
+and ceil((DAILY_FEE*(100-DISCOUNT_RATE))/100)*30<2000000
+order by FEE desc, CAR_TYPE, CAR_ID desc
