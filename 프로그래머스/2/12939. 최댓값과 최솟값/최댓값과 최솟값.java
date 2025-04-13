67ebc22b6685d6fd544de9e425ff9ffef843ae23
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2025-04-13 22:32:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,16 +3,21 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         
-        String[] strArr = s.split(" ");
+        //공백제거 및 String배열로 변환
+        String[] strarr = s.split(" ");
         
-        int[] arr = new int[strArr.length];
+        //String배열을 int배열로 전환
+        int[] arr = new int[strarr.length];
         for(int i=0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(strArr[i]);
+            arr[i] = Integer.parseInt(strarr[i]);
         }
         
+        //오름차순 정렬
         Arrays.sort(arr);
         
+        //정답 형식으로 변환 
         String answer = arr[0] +" "+ arr[arr.length-1];
+        
         return answer;
     }
 }
