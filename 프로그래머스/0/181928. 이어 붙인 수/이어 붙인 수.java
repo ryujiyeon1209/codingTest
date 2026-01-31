1837504e7d23f359e5c05472cd7edec6622d11c1
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2026-01-31 22:35:34 +0900

    [level 0] Title: 이어 붙인 수, Time: 13.23 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        String even = "";
+        String odd = "";
+        
+        for(int num : num_list) {
+            if(num%2==0) even+=num;
+            else odd+=num;
+        }
+        
+        answer+=(Integer.parseInt(even) + Integer.parseInt(odd));
+        return answer;
+    }
+}
