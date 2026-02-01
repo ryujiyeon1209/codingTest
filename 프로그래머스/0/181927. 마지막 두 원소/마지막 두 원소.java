c614edcdf91e1f6b98d2859da7b7500dfe50e20d
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2026-02-01 19:53:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,20 +1,6 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length+1];
-        
-        for(int i=0; i<num_list.length; i++){
-            answer[i] = num_list[i];
-        }
-        
-        int num1 = num_list[num_list.length-1];
-        int num2 = num_list[num_list.length-2];
-        
-        if(num2 < num1) {
-            answer[num_list.length] = num1-num2;
-        } else {
-            answer[num_list.length] = num1*2;
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
