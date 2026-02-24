735bd545e4c3cb2bc056f63b66a90458140bf1bf
zhdwldus <122500615+ryujiyeon1209@users.noreply.github.com>
2026-02-24 22:31:16 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,20 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        
-        for(int i=0; i<num_list.length; i++){
-            int num = num_list[i];
-            
-            while(1<num) {
-                if(num%2==0) num/=2;
-                else num=(num-1)/2;
-                
-                answer++;
-            }
-        }
-        
         return answer;
     }
 }
